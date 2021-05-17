27be0c2d1637fc78efeafc37581260cfa8442fd3
Prateek Mishra <justwriteprateek@gmail.com>
2021-05-17 13:40:55 +0530

    added react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const expenseDashboardPath =()=>(
+    <div>
+        hii this is ExpenseDashBoardPage
+    </div>
+);
+const addExpensePage =()=>(
+    <div>
+        hii this is add Page
+    </div>
+);
+const routes = (
+    <BrowserRouter>
+    <div>
+     <Route path="/" component={expenseDashboardPath} exact = {true}/> 
+     <Route path="/add" component={addExpensePage} exact = {true}/> 
+    </div>
+        
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
